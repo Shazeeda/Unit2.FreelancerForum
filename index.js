7a6031450ff407b4b0e9a3dda402bb57b76983e7
Shazeeda <shazeedamohamed@gmail.com>
2024-11-22 16:28:48 -0700

    changes

--- index.js
+++ index.js
@@ -11,21 +11,22 @@ function init() {
   const heading = document.createElement("h1");
   heading.textContent = "Freelancer Forum";
   rootContainer.appendChild(heading);
+
+  const h2 = document.createElement("table");
+  h2.textContent = "The Average starting price is $30";
+  root.appendChild(h2);
+
+  const availableh1 = document.createElement("h1");
+  availableh1.textContent = "Available Freelancers";
+  root.appendChild(availableh1);
   /**
    * 👉 STEP 3:
    *    Create a table to hold our Freelancers in
    */
 
-  const table = document.createElement("table");
-  const coll = document.createElement("td");
-  col1.textContent = "Name";
-  table.appendChild(col1);
+const table = document.createElement("table");
+root.appendChild(table);
 
-  const tr1 = document.createElement("tr");
-  tr1.textContent = "Bob";
-  table.appendChild(tr1);
-
-  rootContainer.appendChild(table);
 
   /**
    * 👉 STEP 5:
@@ -39,18 +40,42 @@ function init() {
  *    Create a function to render the names in our name array
  * 
  */
-function renderNames() {
-  const namesDiv = document.getElementById('names')
+function freeRender(){
+  const tbody = document.createElement("tbody");
+  table.appendChild(tbody);
+
+
+// hold freelancers
+  const freeRow = doucment.createElement("tr");
+
+  // textcontent goes here
+  const freelancers = documnt.createElement("td");
+
+// this is name, occupation, price
+  const nameColumn1 = document.createElement("th");
+  nameColumn1.textContent = "Name";
+  tbody.appendChild(nameColumn1);
+
+
+  const jobColumn2 = document.createElement("th");
+jobColumn2.textContent = "Occupation";
+tbody.appendChild(jobColumn2);
+
+
+  const priceColumn3 = document.createElement("th");
+  priceColumn3.textContent = "Price"; 
+  tbody.appendChild(priceColumn3);
+
+
 }
-const nameItem = document.createElement('div');
-nameItem.classList.add('name-item');
-renderFreeLancer();
 
 /**
  * 👉 STEP 6:
  *    Create a function to add a new Freelander the Freelancer array
  */
-function addFreelancer(name, occupation, starting price)
+function addNewFreelancer(freelancers){
+  
+}
 
 /**
  * 👉 STEP 7:
@@ -63,3 +88,4 @@ const {name, occupation, starting price} = generateFreelancerData(index);
 addFreelancer{name, occupation, starting price};
 //call init function
 init();
+freeRender();
